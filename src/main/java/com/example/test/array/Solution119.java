7c39573e346009ef473e6d3b114ae0f33ac46779
Bikash Shah <bikashshah15@gmail.com>
2024-06-29 14:01:30 -0500

    119. Pascal's Triangle II

---
+++ src/main/java/com/example/test/array/Solution119.java
@@ -0,0 +1,60 @@
+package com.example.test.array;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution119 {
+    /*
+    119. Pascal's Triangle II
+Solved
+Easy
+Topics
+Companies
+Given an integer rowIndex, return the rowIndexth (0-indexed) row of the Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+
+
+Example 1:
+
+Input: rowIndex = 3
+Output: [1,3,3,1]
+Example 2:
+
+Input: rowIndex = 0
+Output: [1]
+Example 3:
+
+Input: rowIndex = 1
+Output: [1,1]
+
+
+Constraints:
+
+0 <= rowIndex <= 33
+     */
+    public static List<Integer> getRow(int rowIndex) {
+
+        List<Integer> preRow = new ArrayList<>();
+        preRow.add(1);
+        for (int i = 1; i <= rowIndex; i++) {
+            List<Integer> row = new ArrayList<>();
+            row.add(1);
+            for (int j = 1; j < i; j++) {
+                row.add(preRow.get(j-1)+preRow.get(j));
+            }
+            row.add(1);
+
+            preRow = row;
+        }
+        return preRow;
+
+    }
+
+    public static void main(String[] args) {
+        int rowIndex = 3;
+        System.out.println(getRow(rowIndex));
+    }
+}
